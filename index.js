29ca71a82b2cf8ed2a593d66b95b28d345357f88
Tilde <tilde@thuryism.net>
2025-01-30 08:48:49 -0700

    Rename openAIClient

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const path = require("path");
 const fs = require("fs");
-const { generate } = require("./openaiClient");
+const { generate } = require("./openAIClient");
 const bodyParser = require("body-parser");
 
 const app = express();
