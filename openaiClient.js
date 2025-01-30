8ad47a3f7f008769bf1e07dfae945c39983bcbd0
Tilde <tilde@thuryism.net>
2025-01-30 09:02:03 -0700

    Spiffy up the code

--- openaiClient.js
+++ openaiClient.js
@@ -4,57 +4,59 @@ const OpenAI = require("openai");
 const launchDarkly = require("@launchdarkly/node-server-sdk");
 const launchDarklyAI = require("@launchdarkly/server-sdk-ai");
 
+const LD_CONFIG_KEY = "model-upgrade";
+const DEFAULT_CONFIG = {
+  enabled: true,
+  model: { name: "gpt-4" },
+  messages: [],
+};
+
+// In a real app you'd fill in this example data
+const userContext = {
+  kind: "user",
+  name: "Mark",
+  email: "mark.s@lumonindustries.work",
+  key: "example-user-key",
+};
+
 const ldClient = launchDarkly.init(process.env.LAUNCHDARKLY_SDK_KEY);
 const ldAiClient = launchDarklyAI.initAi(ldClient);
 const openaiClient = new OpenAI({ apiKey: process.env.OPENAI_API_KEY });
 
-async function generate(kwargs) {
+async function generate(options = {}) {
   /**
-   * Calls OpenAI's chat completion API to generate some text based on a prompt.
+   * Generates text using OpenAI's chat completion API.
+   * @param {Object} options - Configuration options for the generation
+   * @returns {Promise<string|null>} The generated text or null if an error occurs
    */
-
-  const context = {
-    kind: "user",
-    name: "Sandy",
-    key: "example-user-key",
-  };
-
   try {
-    const aiConfigKey = "diane-tilde-test";
-    const defaultValue = {
-      enabled: true,
-      model: { name: "gpt-4" },
-      messages: [],
-    };
-
     const configValue = await ldAiClient.config(
-      aiConfigKey,
-      context,
-      defaultValue,
-      {
-        ...kwargs,
-      }
+      LD_CONFIG_KEY,
+      userContext,
+      DEFAULT_CONFIG,
+      options
     );
 
-    const modelName = configValue.model.name;
-    console.log("CONFIG VALUE: ", configValue);
-    console.log("MODEL NAME: ", modelName);
-    const { tracker } = configValue;
-    console.log("TRACKER", tracker);
-    const messages = configValue.messages || [];
-    const completion = await tracker.trackOpenAIMetrics(async () => {
-      return await openaiClient.chat.completions.create({
+    const {
+      model: { name: modelName },
+      tracker,
+      messages = [],
+    } = configValue;
+
+    console.log("model: ", configValue.model);
+    const completion = await tracker.trackOpenAIMetrics(async () =>
+      openaiClient.chat.completions.create({
         model: modelName,
         messages,
-      });
-    });
+      })
+    );
 
     const response = completion.choices[0].message.content;
-    console.log("Success.");
-    console.log("AI Response:", response);
+
     return response;
   } catch (error) {
-    console.error(error);
+    console.error("Error generating AI response:", error);
+    return null; // or throw error depending on your error handling strategy
   }
 }
 
